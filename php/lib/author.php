10029443f0c7ecfa60f20606c2ef612291a05e88
opeyemi-jonah <gavrieljonah@gmail.com>
2020-04-07 22:01:41 -0600

    completed class Author... <time to rest>Zzz!

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,11 +1,12 @@
 <?php
 
 use OpeyemiJonah\ObjectOriented\Author;
-
+require ("uuid.php");
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once ("../classes/Author.php");
 $secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/ojonah.ini");
  $pdo = $secrets->getPdoObject();
+
 //require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 require_once (dirname(__DIR__,1)."/classes/Author.php");
@@ -14,25 +15,25 @@ require_once (dirname(__DIR__,1)."/classes/Author.php");
 $password = "$\skull_skunk_%year";
 $authorHash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 45]);
 
-		//$authorId = "3134e90a-e3a5-4df2-abff-7cc7d8324530";
+		$authorId = "3f49fd80-2e13-40f5-bb34-ba5c7359e7b2";
 
-		$authorActivationToken = 'o9AbabiSlayerjkGE9xo9ZFoTGE9x750';
+$authorActivationToken = bin2hex(random_bytes(16));
 
-		$authorAvatarUrl = "https://avars.discourse.org/v4/letter/m/a8b319/squad4.png";
+		$authorAvatarUrl = "https://avars.discourse.org/v4/letter/m/a8b319/newHomie90.png";
 
-		$authorUsername = "Andre3000";
+		$authorUsername = "Ameer";
 		
-		$authorEmail = "Aundre@cnm.edu";
+		$authorEmail = "Kilo@gnail.edu";
 
-		$author = new Author(generateUuidV4(), $authorActivationToken, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
+		$author = new Author($authorId, $authorActivationToken, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
 
-//$author->insert($pdo);
+$author->insert($pdo);
 echo " Here"."<br>";
 echo "hash: ".$author->getAuthorHash();
 
 //print_r($getSingleObj);
 
-
+/*
 $email = $author->getAuthorByEmail($pdo,$authorEmail);
 
 echo " this is the result: ".'<br>';
@@ -48,6 +49,4 @@ print_r($email);
 		//$author->delete($pdo);
 		//Author::getAllAuthor($pdo);
 	//$authors->getAllAuthor($pdo);
-
-
-
+*/
