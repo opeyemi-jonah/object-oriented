82b8d17ec831bb9f64c88b2cc8c6c205a75b9af6
opeyemi-jonah <gavrieljonah@gmail.com>
2020-04-07 22:21:53 -0600

    clean up

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,16 +1,11 @@
 <?php
-
 use OpeyemiJonah\ObjectOriented\Author;
 require ("uuid.php");
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
-require_once ("../classes/Author.php");
 $secrets =  new Secrets("/etc/apache2/capstone-mysql/cohort28/ojonah.ini");
  $pdo = $secrets->getPdoObject();
 
-//require_once("/etc/apache2/capstone-mysql/Secrets.php");
-
 require_once (dirname(__DIR__,1)."/classes/Author.php");
-//use Author;
 
 $password = "$\skull_skunk_%year";
 $authorHash = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 45]);
@@ -27,25 +22,18 @@ $authorActivationToken = bin2hex(random_bytes(16));
 
 		$author = new Author($authorId, $authorActivationToken, $authorAvatarUrl, $authorEmail, $authorHash, $authorUsername);
 
-$author->getAuthorByUsername($pdo,"A");
+//$author->getAuthorByUsername($pdo,"A");
 echo " Here"."<br>";
 echo "hash: ".$author->getAuthorHash();
 
-//print_r($getSingleObj);
-
 /*
 $email = $author->getAuthorByEmail($pdo,$authorEmail);
-
 echo " this is the result: ".'<br>';
-
 var_dump($email);
 print_r($email);
-
 //$auth = $author->getAuthor($pdo,$authorId);
-
 //$authors = Author::getAllAuthor($pdo);
 //$authors =  $author->getAllAuthor($pdo);
-
 		//$author->delete($pdo);
 		//Author::getAllAuthor($pdo);
 	//$authors->getAllAuthor($pdo);
