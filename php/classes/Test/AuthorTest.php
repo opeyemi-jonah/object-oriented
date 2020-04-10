405e59b23de922162f80c587d981ff5d36eb4687
opeyemi-jonah <gavrieljonah@gmail.com>
2020-04-10 10:58:17 -0600

    debugged and added self::assertEquals to make sure we getting back the right data after test run

--- php/classes/Test/AuthorTest.php
+++ php/classes/Test/AuthorTest.php
@@ -69,9 +69,25 @@ $author->insert($this->getPDO());
 			$this->VALID_AUTHOR_HASH,
 			$this->VALID_USERNAME);
 		$author->insert($this->getPDO());
+
+		//update a value on the record I just inserted
+		$changedAunthorUsername = $this->VALID_USERNAME."changed";
+		$author->setAuthorUsername($changedAunthorUsername);
+		$author->update($this->getPDO());
+
 		//check count of author record in the db after the insert
 		$numRowsAfterInsert = $this->getConnection()->getRowCount("author");
 		self::assertEquals($numRows + 1, $numRowsAfterInsert,"update checked record count");
+		//get a copy of the record just inserted and validate the values
+		// make sure the values that went into the record are the same ones that come out
+		$pdoAuthor = Author::getAuthorByAuthorId($this->getPDO(), $author->getAuthorId()->getBytes());
+		self::assertEquals($authorId,$pdoAuthor->getAuthorId());
+		self::assertEquals($this->VALID_ACTIVATION_TOKEN, $pdoAuthor->getAuthorActivationToken());
+		self::assertEquals($this->VALID_AVATAR_URL, $pdoAuthor->getAuthorAvatarUrl());
+		self::assertEquals($this->VALID_AUTHOR_EMAIL, $pdoAuthor->getAuthorEmail());
+		self::assertEquals($this->VALID_AUTHOR_HASH, $pdoAuthor->getAuthorHash());
+		//verify that the saved username is same as the updated username
+		self::assertEquals($changedAunthorUsername, $pdoAuthor->getAuthorUsername());
 
 	}
 
